d6da2f88d966987ec5ae79e328994033e05e332f
Utsav Raj <utsavrajdocx@gmail.com>
2025-12-05 18:35:12 +0530

    Delete Review Added + After Deleting the listing,corresponding  reviews will also be deleted

--- app.js
+++ app.js
@@ -127,6 +127,15 @@ app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res) => {
     res.redirect(`/listings/${listing._id}`);
 }));
 
+//Delete Route
+app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async(req,res)=>{
+    let {id ,reviewId} = req.params;
+    await Listing.findByIdAndUpdate(id , {$pull: {reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
+}));
+
+
 //Show Route
 app.get("/listings/:id",wrapAsync( async (req,res)=>{
     let {id} = req.params;
--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 //Schema
 
@@ -37,6 +38,12 @@ const listingSchema = new Schema({
 
 });
 
+
+listingSchema.post("findOneAndDelete" ,async(listing) =>{
+  if(listing){
+      await Review.deleteMany({_id: {$in: listing.reviews}});
+  };
+}); 
 
 //model
 
